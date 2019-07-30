9122de616b128823f2923c402be802a8f49542b9
Kartik Saraf <kartiksaraf9@gmail.com>
2019-07-31 01:39:48 +0530

    Added example for Video Slider Gallery View

--- app/src/main/java/com/zeuskartik/mediasliderview/ImageSliderDemo.java
+++ app/src/main/java/com/zeuskartik/mediasliderview/ImageSliderDemo.java
@@ -6,16 +6,16 @@ import com.zeuskartik.mediaslider.MediaSliderActivity;
 
 import java.util.ArrayList;
 
-public class MediaSliderDemo extends MediaSliderActivity {
+public class ImageSliderDemo extends MediaSliderActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ArrayList<String> list = new ArrayList<>();
-        list.add(https://res.cloudinary.com/kartiksaraf/image/upload/v1564514468/github_MediaSliderView/demo_images/8-phone-wallpaper_gcseap.jpg);
+        list.add("https://res.cloudinary.com/kartiksaraf/image/upload/v1564514468/github_MediaSliderView/demo_images/8-phone-wallpaper_gcseap.jpg");
         list.add("https://res.cloudinary.com/kartiksaraf/image/upload/v1564514549/github_MediaSliderView/demo_images/ea0ef44d800aa07722c25b1a6db58800--iphone-backgrounds-phone-wallpapers_cqmbbx.jpg");
         list.add("https://res.cloudinary.com/kartiksaraf/image/upload/v1564514590/github_MediaSliderView/demo_images/Quotefancy-20588-3840x2160_msurjx.jpg");
         list.add("https://res.cloudinary.com/kartiksaraf/image/upload/v1564514634/github_MediaSliderView/demo_images/Quotefancy-2098-3840x2160_nrez6k.jpg");
-        list.add("https://res.cloudinary.com/kartiksaraf/image/upload/v1564514699/github_MediaSliderView/demo_images/download_totbb2.jpg")
-        loadMediaSliderView(list,"image",true,true,false,"Media-Slider","#000000",null);
+        list.add("https://res.cloudinary.com/kartiksaraf/image/upload/v1564514699/github_MediaSliderView/demo_images/download_totbb2.jpg");
+        loadMediaSliderView(list,"image",true,true,false,"Image-Slider","#000000",null);
     }
 }
--- app/src/main/java/com/zeuskartik/mediasliderview/MainActivity.java
+++ app/src/main/java/com/zeuskartik/mediasliderview/MainActivity.java
@@ -8,17 +8,18 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv;
+    TextView tv,tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.texter);
+        tv = findViewById(R.id.launch_image);
+        tv1 = findViewById(R.id.launch_video);
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,MediaSliderDemo.class));
+                startActivity(new Intent(MainActivity.this,ImageSliderDemo.class));
             }
         });
     }
